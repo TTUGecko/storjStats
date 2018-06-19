d5a9bd3f568138bf39220b9ce40005bbee222c82
TTUGecko <31365056+TTUGecko@users.noreply.github.com>
2018-06-18 21:39:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ import os
 
 logPath = "***Replace with the fully qualified path to Storj Logs***"
 timezone = "***Replace with local timezone***"
+installDir = "**Fully qualified path to the directory where you insall storjStats**" #Ex: "/home/<user>/storjStats"
+
 '''
 Sample Acceptable Timezones example:
 'America/Chicago'
@@ -37,7 +39,7 @@ nodes = set([x[:x.find('_')] for x in os.listdir(logPath) if '.log' in x])
 from logger import nodeLogData
 
 try:
-    nodeDF = pd.read_csv('outputs/nodeDF.csv',sep='|',index_col=0,parse_dates=True,infer_datetime_format=True)
+    nodeDF = pd.read_csv(installDir + '/outputs/nodeDF.csv',sep='|',index_col=0,parse_dates=True,infer_datetime_format=True)
     nodeDF.index = nodeDF.index.tz_localize('UTC')
     
 except FileNotFoundError:
@@ -49,11 +51,11 @@ except FileNotFoundError:
         except NameError:
             nodeDF = nodeData.lines
         nodeDF = nodeDF.sort_index()
-        nodeDF.reset_index().drop_duplicates().to_csv('outputs/nodeDF.csv',index=False,sep='|')
+        nodeDF.reset_index().drop_duplicates().to_csv(installDir + '/outputs/nodeDF.csv',index=False,sep='|')
 
 for node in nodes:
     nodeData = nodeLogData(node,logPath,pd.to_datetime(nodeDF.index.max().tz_convert(timezone).date()))
     nodeData.decodeLines()
     nodeDF = nodeDF.append(nodeData.lines)
     nodeDF = nodeDF.sort_index()
-    nodeDF.reset_index().drop_duplicates().to_csv('outputs/nodeDF.csv',index=False,sep='|')
+    nodeDF.reset_index().drop_duplicates().to_csv(installDir + '/outputs/nodeDF.csv',index=False,sep='|')
